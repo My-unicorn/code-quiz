0975456f69e5bc9ada7ffd0a9dfac638dc4d6b81
Araya Nawrath <arayanawrath@gmail.com>
2020-03-07 10:20:45 -0500

    updated

---
+++ quiz.js
@@ -0,0 +1,27 @@
+var quizQs = [
+    {
+        title: "1.Which of the following functions of Number object would display output in exponential format: ",
+        choices: ["toPrecision()", "toFixed()", "toExponential()", "toLocaleString()"],
+        answer: "toExponential()"
+    },
+    {
+        title: "2.Select a function of Array object which returns a new array comprised of the current array /or its value(s)?: ",
+        choices: ["some()", "push()", "pop()", "concat()"],
+        answer: "concat()"
+    },
+    {
+        title: "3.Which of the following functions of Number object would return a string version of the number that may change according to the browser’s locale settings?: ",
+        choices: ["toLocaleString()", "toExponential()", "toString()", "toFixed()"],
+        answer: "toLocaleString()"
+    },
+    {
+        title: "4.Which of the following methods removes the last element from an array and returns that element?: ",
+        choices: ["last()", "pop()", "get()", "None"],
+        answer: "pop()"
+    },
+    {
+        title: "5.Which of the following function of String object returns the capitalized string while respecting the current locale?: ",
+        choices: ["toLocaleUpperCase()", "toUpperCase()", "substring()", "toString()"],
+        answer: "toLocaleUpperCase()"
+    },
+  ];
